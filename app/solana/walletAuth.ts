152585ef485de5bcfa7a6352b699794aa989a1a2
akash patel <akash906129@gmail.com>
2026-02-16 23:29:44 +0530

    sign Message fixed

--- app/solana/walletAuth.ts
+++ app/solana/walletAuth.ts
@@ -2,37 +2,51 @@ import {
     transact,
     Web3MobileWallet,
 } from "@solana-mobile/mobile-wallet-adapter-protocol-web3js";
-import type {
-    SolanaSignInInput,
-    SolanaSignInOutput,
-} from "@solana/wallet-standard-features";
-import { verifySignIn } from "@solana/wallet-standard-util";
-import { Buffer } from 'buffer';
-import { APP_IDENTITY, SIGN_IN_PAYLOAD } from "./config";
-import { stringToUint8Array } from "../utils";
 import axios from "axios"
 
-window.Buffer = Buffer;
+const APP_IDENTITY = {
+    name: "Bits",
+    uri: "https://bits.app",
+    icon: "favicon.ico",
+};
+
 
 async function connectWallet() {
     return await transact(async (wallet: Web3MobileWallet) => {
         try {
-            console.log("before connect");
             const authorizationResult = await wallet.authorize({
                 chain: 'solana:devnet',
                 identity: APP_IDENTITY,
-                // sign_in_payload: SIGN_IN_PAYLOAD
             });
-
-            console.log("after connect");
-
             if (!authorizationResult.accounts[0].address) {
                 throw new Error("no address returned from wallet")
             }
-
             return {
                 address: authorizationResult.accounts[0].address
             };
+        } catch (e: any) {
+            console.log(e);
+        }
+    })
+}
+
+async function signNonceMessage(nonce: string, address: string) {
+    return await transact(async (wallet: Web3MobileWallet) => {
+        try {
+            const message = 'Please sign this message to verify your identity. Nonce: ' + nonce;
+            const messageBuffer = new Uint8Array(
+                message.split('').map(c => c.charCodeAt(0)),
+            );
+
+            const signedMessages = wallet.signMessages({
+                addresses: [address],
+                payloads: [messageBuffer]
+            })
+
+            console.log("signedMessages ", signedMessages);
+
+            return signedMessages;
+
         } catch (e: any) {
             console.log(e);
         }
@@ -47,7 +61,14 @@ export async function checkUserExist() {
         const res2 = await axios.post("http://10.67.120.95:3000/api/v1/user/check", {
             "publicKey": res.address
         })
-        console.log(res2.data);
+        // console.log(res2`.data);
+
+        console.log("before calling nonce");
+
+        const nonceRes = await signNonceMessage(res2.data.nonce, res.address);
+
+        console.log("before calling nonce");
+        console.log(nonceRes);
 
     } catch (e) {
         console.log(e);
